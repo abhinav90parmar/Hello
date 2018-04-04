39a96509d4b14179dbef0320ee68710ef3c57a1c
RUTWI <RUTWI@RUTWI-PC>
2018-04-04 08:22:16 -0700

    First Git Commit.

---
+++ Hello/src/com/code/Hello.java
@@ -0,0 +1,16 @@
+package com.code;
+
+public class Hello {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("Hello from Eclipse Oxygen");
+
+		for (int i = 0; i < args.length; i++) {
+			
+		}
+		
+	}
+
+}
